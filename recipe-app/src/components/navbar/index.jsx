b5de6785fdc39d15a5604cac39d80a1f8e68c5e4
Jenkibear <jenkinwork123@gmail.com>
2024-04-18 15:11:31 +1000

    NavLink the pages so they're acessible by pressing the button in navbar

--- recipe-app/src/components/navbar/index.jsx
+++ recipe-app/src/components/navbar/index.jsx
@@ -1,10 +1,12 @@
 
-
+import {NavLink} from 'react-router-dom';
 
 
 export default function Navbar() {
-    return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-roq gap-5 lg:gap-0">
-        <h2 className="text-2xl font-semibold">Food Recipe</h2>
+    return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
+        <h2 className="text-2xl font-semibold">
+        <NavLink to={'/'} className='text-black hover:text-gray-700 duration-300'>Food Recipe</NavLink>
+        </h2>
         <form>
             <input
                 type="text"
@@ -13,6 +15,15 @@ export default function Navbar() {
                 className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
             />
         </form>
+        <ul className="flex gap-5">
+            <li>
+                <NavLink to={'/'} className='text-black hover:text-gray-700 duration-300'>Home</NavLink>
+            </li>
+            <li>
+                <NavLink to={'/favourites'} className='text-black hover:text-gray-700 duration-300'>Favourites</NavLink>
+            </li>
+
+        </ul>
 
     </nav>
 }
