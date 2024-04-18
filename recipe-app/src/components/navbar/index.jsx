629f38c63ccc71761433657d809717ec7327c45a
Jenkibear <jenkinwork123@gmail.com>
2024-04-18 15:18:17 +1000

    set up global context for search paramter
    once values put in search bar theyre sent to global context and then global searchParam exists

--- recipe-app/src/components/navbar/index.jsx
+++ recipe-app/src/components/navbar/index.jsx
@@ -1,8 +1,16 @@
 
+import { useContext } from 'react';
 import {NavLink} from 'react-router-dom';
+import { GlobalContext } from '../../context';
 
 
 export default function Navbar() {
+
+    const {searchParam,setSearchParam}= useContext(GlobalContext);
+
+    console.log(searchParam);
+
+
     return <nav className="flex justify-between items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
         <h2 className="text-2xl font-semibold">
         <NavLink to={'/'} className='text-black hover:text-gray-700 duration-300'>Food Recipe</NavLink>
@@ -11,6 +19,8 @@ export default function Navbar() {
             <input
                 type="text"
                 name="search"
+                value={searchParam}
+                onChange={(event)=>setSearchParam(event.target.value)}
                 placeholder="Enter Items..."
                 className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200"
             />
